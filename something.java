f7301d6e1f3da5b962dc2e919a16c9317fa34cce
Anna Pedersen <282349J@curtin.edu.au>
2024-10-27 14:56:33 +0800

    started the stock market

--- something.java
+++ something.java
@@ -1,38 +1,52 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class something {
     public static void help()  {
-        System.out.println("Help menu?");
+        System.out.println("Commands");
+    }
+
+
+
+    public static void stock(snickerdoodles snickerdoodles_object, int snickerdoodles_price_int)  {
+        snickerdoodles_object = snickerdoodles_object.set_price(snickerdoodles_object.price + snickerdoodles_price_int);
+        System.out.println("SnickerDoodle stock price" + snickerdoodles_object.price);
     }
     
+
+
     public static void main(String[] args)  {
-        
-        snickerdoodles snickerdoodles_object = new snickerdoodles(5,10);
-        snickerdoodles_object.set_price(5);
-        System.out.println(snickerdoodles_object.price);
-
-        
-
+        boolean game_active = true;
         int cookies = 0;
-        int[] cookie_types = {1,2,3,4,5};
         
+        int depression = 0;
         boolean depressed = false;
-        
-        for (int i=0; i<cookie_types.length; i++)    {
-            cookies = cookies + cookie_types[i];
+        snickerdoodles snickerdoodles_object = new snickerdoodles(5,10);
+        //snickerdoodles_object.set_price(5);
+        //System.out.println(snickerdoodles_object.price);
+
+
+        //for (int i=0; i<cookie_types.length; i++)    {
+        //    cookies = cookies + cookie_types[i];
+        //}
+
+        while (game_active == true)  {
+            Random snickerdoodle_price = new Random();
+
+            Scanner playerinput = new Scanner(System.in);  // Create a Scanner object
+            String PInput = playerinput.nextLine();  // Read user input
+            
+
+            if (PInput == "help")   {
+                help();
+            }   else if (PInput == "help")  {
+                int snickerdoodles_price_int = snickerdoodle_price.nextInt(10);
+                stock(snickerdoodles_object,snickerdoodles_price_int);
+            }   else    {
+                System.out.println("Invalid command, use help to know valid commands");
+            }
+            
         }
-
-
-        String cookies_text = "You Have " + cookies + " cookies";
-        System.out.println(cookies_text);
-        if (cookies == 0 && depressed == true)   {
-            System.out.println("You are depressed and have 0 cookies");
-        }   else if (cookies > 0 && depressed == true)  {
-            System.out.println("You have " + cookies + " and are depressed");
-        }   else  {
-            System.out.println("You have " + cookies + " and are happy");
-        }
-
-        help();
-
     }
     
 }
